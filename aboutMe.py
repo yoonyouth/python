af968b2980b3d963beba59f79c1dcbdd2ee97dd6
yoonyouth <yoon.youth.cs@gmail.com>
2021-02-21 19:49:57 -0500

    Update aboutMe.py

--- aboutMe.py
+++ aboutMe.py
@@ -1,4 +1,8 @@
 first_name = '윤'
 last_name = '젊음'
+age = 26
+hobby = '낚시'
 
-print('제 이름은 '+ first_name + ' ' + last_name + '입니다.')
+print('제 이름은 ' + first_name + ' ' + last_name + '입니다.')
+print('나이는 ' + str(age) + '살 이고, 취미는 ' + hobby + '입니다.')
+#print('나이는 %d살 이고, 취미는 %s입니다.'%(age,hobby))
