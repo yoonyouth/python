2bd68c6a70fd49414a943517714a117b37fa9b15
yoonyouth <yoon.youth.cs@gmail.com>
2021-02-22 17:51:54 -0500

    Update list.py

--- list.py
+++ list.py
@@ -1,1 +1,20 @@
 splatoon = ['Yoon','Hong-si','Neseki']
+
+print(splatoon)
+print(len(splatoon))
+print(splatoon[2])
+
+#add a new item at the end of the list
+splatoon.append('Minji')
+
+print(splatoon)
+print(len(splatoon))
+print(splatoon[2])
+
+#remove the 'Minji' item
+splatoon.remove('Minji')
+
+print(splatoon)
+print(len(splatoon))
+print(splatoon[2])
+
