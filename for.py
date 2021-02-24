b644cb4e5e708b5417d2e5194fab6e0fde9b7691
yoonyouth <yoon.youth.cs@gmail.com>
2021-02-23 23:02:27 -0500

    Update for.py

--- for.py
+++ for.py
@@ -2,6 +2,8 @@
     # for countdown in [0,1,2,3,4]:
     #     print('카운트 다운: {0}'.format(countdown))
 
-for countdown in [4,3,2,1,0]:
-    print('카운트 다운: {0}'.format(countdown))
+# for countdown in [4,3,2,1,0]:
+#     print('카운트 다운: {0}'.format(countdown))
 
+for countdown in range(0,5):
+    print('카운트 다운: {0}'.format(countdown))
