cf7006cc3cd83692028eed81f7a8471ccc5b99d9
yoonyouth <yoon.youth.cs@gmail.com>
2021-02-23 23:22:59 -0500

    Update the guessWhat.py

---
+++ guessWhat.py
@@ -0,0 +1,12 @@
+a = [15,25,35]
+print(a[2]+a[0])
+
+print(a[1]*a[0])
+
+number = [1,2,3,4,5,6,7,8,9,10]
+print(number[1:3])
+print(number[3:])
+print(number[:3])
+print(number[1:3:2]) #every number between index 1 to index 3 per 2
+print(number[0:9:2]) #0~9 per 2
+print(number[::-1]) #reverse
